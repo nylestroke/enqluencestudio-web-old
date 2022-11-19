1b8884470eb2d1ef3947bd2a806ac79ee7ad8dc0
Vladyslav Potapenko <contact@nylestroke.me>
2022-11-19 23:57:36 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import MainPage from "./pages/MainPage/MainPage";
 import Preloader from "./components/Preloader/Preloader";
 import Footer from "./components/Footer/Footer";
-import OurTeam from './pages/OurTeam/OurTeam';
+import OurTeam from "./pages/OurTeam/OurTeam";
 import Contact from "./pages/Contact/Contact";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Preloader/>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<MainPage/>}/>
-                <Route path="/ourteam" element={<OurTeam/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-            </Routes>
-            <Footer/>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Preloader />
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/ourteam" element={<OurTeam />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
